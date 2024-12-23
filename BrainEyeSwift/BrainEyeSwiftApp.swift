b11769fb8c3233a59ac27c230473450ab983346d
PaulMaltsev <maltsevpavel95@gmail.com>
2024-12-23 10:06:43 +0200

    Initial Commit

---
+++ BrainEyeSwift/BrainEyeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainEyeSwiftApp.swift
+//  BrainEyeSwift
+//
+//  Created by Paul on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrainEyeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
